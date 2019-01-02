b7c07352305f7d468530a7fb5974aa7be3a713f8
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 02:22:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ end
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  puts ""
-
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name
+  return position
 end
